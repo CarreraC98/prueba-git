9a4c0e9a4a9087923f624c61a0411997dd5b8bb1
CarreraC <c.carreraordaz@gmail.com>
2024-10-11 18:15:48 -0300

    este es mi segundo commit

---
+++ hellogit2.java
@@ -0,0 +1,22 @@
+public class hellogit2 {
+    public static void main(String[] args) {
+
+        // Variable
+        var miVariable = "variable";
+
+        // Constante
+        final String miConstante = "Constante";
+
+        // Datos primitivos
+        byte datoPequeño = 8; // Almaneca valores pequeños
+        short datoMasGrandeQueByte = 120; // Almcaena mas datos que byte
+        int miEntero = 2000; // Es el mas comun
+        long miEnteroGrande = 200000; // Es para almacenar enteros mas grandes
+        double miFlotante = 120.5d; // Se usa para almacenar valores flotantes, mas precision
+        float miDecimal = 120.6f; // Se usa para almacenar valores flotantes, menos precision
+        char miChat = 'a'; // Se usa para almacenar caracteres (unicode) con comillas simples
+        boolean miBoolean = true; // Se usa para almacenar valores de verdad (true / false)
+
+        System.out.println("¡Hola, Java ");
+    }
+}
